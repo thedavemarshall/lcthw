220087f9525f91a3c04e63bb464868af6d4cec71
Dave Marshall <depmarshall@gmail.com>
2014-01-23 17:19:33 -0800

    Ex4 valgrind

--- Makefile
+++ Makefile
@@ -1,6 +1,14 @@
 CFLAGS=-Wall -g
 
 clean:
-	rm -f ex1 ex3
+	rm -f ex1 ex3 ex4
 
-all: ex1 ex3
+all: ex1 ex3 ex4
+
+val:
+	make all
+	valgrind ./ex*
+
+reval:
+	make clean
+	make val
---
+++ ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose. */
+
+int main()
+{
+  int age = 23;
+  int height = 68;
+  
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n", height);
+  
+  return 0;
+}
